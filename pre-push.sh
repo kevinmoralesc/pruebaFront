b80b38637a3e53a4ee31dc51290c8d40aa25e5c8
kevinmoralesc <kevina.moralesc@uqvirtual.edu.co>
2024-05-21 19:15:53 -0500

    fallo 2

--- pre-push.sh
+++ pre-push.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Verificar si hay commits en la rama actual antes de intentar retroceder
-if [ "$(git rev-list --count HEAD)" -gt 1 ]; then
-    # Revertir los cambios locales
-    echo "Revertir los cambios locales."
-    git reset --hard HEAD~1
-else
-    echo "No hay commits disponibles antes de HEAD. No se pueden revertir los cambios."
-fi
+# Revertir los cambios locales
+echo "Revertir los cambios locales."
+git reset --hard HEAD~1
